b277e0ba97c1dd119dcaaae066625d96be1532eb
Jamar Torres <Jetorres88@gmail.com>
2019-11-24 21:31:36 -0500

    completed list comprehension challenge

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,48 +24,48 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [idx.name for idx in humans if idx.name[0] == 'D']
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [end.name for end in humans if end.name[-1:] == 'e']
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [letter.name for letter in humans if letter.name[0] >= 'C' and letter.name[0] <= 'G']
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [ a.age + 10 for a in humans]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
+e = [(s.name + '-' + str(s.age)) for s in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [(t.name, t.age) for t in humans if t.age > 26 and t.age < 33]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = [Human(u.name.upper(), u.age + 5) for u in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [math.sqrt(sq.age) for sq in humans]
 print(h)
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -50,5 +50,5 @@ vehicles = [
 
 # Go through the vehicles list and print the result of calling drive() on each.
 for d in vehicles:
-    print(d.drive())
+    print( f'Vehicle drive sound: ', d.drive())
 # TODO
